d14dd7080ce4c10a97559596354467409b7af325
Thiago Andrade <thiagoadd14@gmail.com>
2024-10-07 14:20:34 -0300

    tipos

---
+++ aula 2 (variaveis e tipos de dados/main.js
@@ -0,0 +1,41 @@
+/*      VARIÁVEIS
+
+const = depois de criada não é possivel aterar o valor depois de declarada (constante)
+
+let = essa variável é possivel alterar o valor
+
+var = é a maneira mais antiga de declarar uma variável
+
+*/
+
+
+
+/*      TIPOS DE DADOS
+
+string = texto usado dentro de aspas ou crase
+
+Numbers = Números
+
+Boolean = true or false
+
+Objects = é uma forma de agrupar várias informações ex:
+
+const usuario = {
+    nome: 'Thiago',
+    idade: 23,
+}
+caso queria um dado dentro do objeto é usado o comando= nomedoobjeto.dado ex: usuario.nome
+
+Null & Undefined = Null: Nulo, sem valor. Undefined: quando ocorre um erro no codigo ex: usuario.local
+
+Array = Vetor uma variavel com varios espaços 
+
+const array = [
+nome: 'Thiago',
+    idade: 23,
+},
+nome: 'Davi',
+    idade: 8,
+}]
+*/
+
